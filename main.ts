648dbd8c29bc1972841b56a0e903f2786db6c551
fukui295 <136450187+fukui295@users.noreply.github.com>
2025-06-25 14:29:12 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 let 動作時間 = 0
+let 秒時間 = 0
 basic.forever(function () {
     動作時間 = input.runningTime()
-    serial.writeLine("" + (動作時間))
+    秒時間 = Math.trunc(動作時間 / 1000)
+    basic.showNumber(秒時間)
+    serial.writeNumbers([動作時間, 秒時間])
 })
